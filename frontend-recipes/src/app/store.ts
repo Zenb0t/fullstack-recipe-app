c8de4039eb071df2a1a32f9ef87075d4363e9f66
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-01-04 07:41:05 -0800

    refactor

--- frontend-recipes/src/app/store.ts
+++ frontend-recipes/src/app/store.ts
@@ -1,11 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import recipesReducer from '../features/recipe/RecipeSlice';
+import recipesReducer from '../features/recipeBook/RecipeSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    recipe: recipesReducer,
+    recipeBook: recipesReducer,
   },
 });
 
