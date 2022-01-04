54e60993698d55756d7791456986813574231e87
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-01-04 07:39:55 -0800

    small refactor

--- frontend-recipes/src/AppRouter.tsx
+++ frontend-recipes/src/AppRouter.tsx
@@ -1,26 +1,36 @@
-import { Routes, Route } from "react-router-dom";
-import RecipeCardList from "./features/recipe/RecipeCardList";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import RecipeCardList from "./features/recipeBook/RecipeCardList";
 import { useAppSelector } from "./app/hooks";
-import { selectRecipes, selectFavoriteRecipes } from "./features/recipe/RecipeSlice";
+import { selectRecipes, selectFavoriteRecipes } from "./features/recipeBook/RecipeSlice";
 import App from "./App";
+import RecipeDetail from "./features/recipeBook/RecipeDetail";
+import { RecipeForm } from "./features/recipeBook/RecipeForm";
 
-
+/**Contains the routes for the application */
 export default function AppRouter() {
 
+    const navigate = useNavigate();
+
+    const AllRecipes = () => <RecipeCardList recipes={useAppSelector(selectRecipes)} />;
+
+    const Favorites = () => <RecipeCardList recipes={useAppSelector(selectFavoriteRecipes)} />;
+    
+    const WithEgg = () => <div>With Eggs</div>;
+    
+    const NoMatch = () => <div>No match, try a different URL</div>;
+    
+    const AddRecipe = () => <RecipeForm handleClose={() => navigate(`/allrecipes`, { replace: true })}/>;
 
     return (
         <Routes>
             <Route path="/" element={<App />} >
+                <Route path="add-recipe" element={<AddRecipe />} />
                 <Route path="/allrecipes" element={<AllRecipes />} />
                 <Route path="/favorites" element={<Favorites />} />
                 <Route path="/withEgg" element={<WithEgg />} />
+                <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
+                <Route path="*" element={<NoMatch/>} />
             </Route>
         </Routes>);
 
 }
-
-const AllRecipes = () => <RecipeCardList recipes={useAppSelector(selectRecipes)} />;
-
-const Favorites = () => <RecipeCardList recipes={useAppSelector(selectFavoriteRecipes)} />;
-
-const WithEgg = () => <div>With Eggs</div>;
--- frontend-recipes/src/fakeData.tsx
+++ frontend-recipes/src/fakeData.tsx
@@ -1,20 +1,21 @@
-import RecipeModel from "./features/recipe/RecipeModel";
+import RecipeModel from "./features/recipeBook/RecipeModel";
+import { v4 as uuidv4 } from 'uuid';
 
 
-export default function fakeData() {
-
-    const imagesrc = 'https://www.eatwell101.com/wp-content/uploads/2020/02/chicken-soup-recipe-3.jpg';
+export default function genRecipe() {
+    const recipe: RecipeModel = {
+        title: 'Recipe Title',
+        description: description,
+        ingredients: ['Ingredient1', 'Ingredient2'],
+        totalTime: '1h 30min',
+        instructions: ['First Step', 'Second Step', 'Third Step'],
+        imageUrl: imagesrc,
+        favorite: true,
+        id: uuidv4(),
+    };
+    return recipe;
 
 }
-
+const description = `Bacon ipsum dolor amet rump burgdoggen bacon filet mignon, spare ribs ribeye ham meatball.
+ Alcatra capicola landjaeger pork chop, filet mignon tenderloin chislic meatloaf spare ribs flank meatball. Corned beef chislic drumstick capicola sausage.`;
 const imagesrc = 'https://www.eatwell101.com/wp-content/uploads/2020/02/chicken-soup-recipe-3.jpg';
-
-export const recipe: RecipeModel = {
-    title: 'Recipe Title',
-    description: 'Recipe Description',
-    ingredients: 'Recipe Ingredients',
-    instructions: 'Recipe Instructions',
-    imageUrl: imagesrc,
-    favorite: true,
-    id: '1',
-};
